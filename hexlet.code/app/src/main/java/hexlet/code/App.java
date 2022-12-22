1b981ee1c6105a13f05b278fa3389ec6c0dc93fc
Daukaevt <mupbam1@gmail.com>
2022-12-22 23:42:17 +0300

    new commandline added

--- hexlet.code/app/src/main/java/hexlet/code/App.java
+++ hexlet.code/app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+
+import picocli.CommandLine;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new CommandLine(new GeneretingDiffer()).execute(args);
+    }
+}
+//@CommandLine.Command(name="gendiff")
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class GeneretingDiffer implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello, picocli!");
     }
 }
