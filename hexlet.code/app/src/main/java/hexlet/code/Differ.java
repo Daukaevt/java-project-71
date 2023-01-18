23d4dcf80770e6fcec52abca5c9b0ccc5a2afbda
Daukaevt <mupbam1@gmail.com>
2023-01-18 17:35:21 +0300

    nested array testing

--- hexlet.code/app/src/main/java/hexlet/code/Differ.java
+++ hexlet.code/app/src/main/java/hexlet/code/Differ.java
@@ -8,10 +8,12 @@ public class Differ {
         String contentFile1 = null;
         try {
             contentFile1 = ReadFile.read(filePath1);
+            //contentFile1 = ReadFile.read("testFile");//testFile
+           // System.out.println(contentFile1 + " test");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        var parseJson1 = JacksonObjectMapperMakingHostAddresses.parse(contentFile1);
+        var parseJson1 = JacksonObjectMapperMakingHostAddresses.parse(String.valueOf(contentFile1));
         String contentFile2 = null;
         try {
             contentFile2 = ReadFile.read(filePath2);
