d9075f790fea09ece37528778a89e4fc886ad70c
Daukaevt <mupbam1@gmail.com>
2023-01-22 05:57:48 +0300

    parsingInit method added

--- hexlet.code/app/src/main/java/hexlet/code/Differ.java
+++ hexlet.code/app/src/main/java/hexlet/code/Differ.java
@@ -1,29 +1,34 @@
 package hexlet.code;
 
+import hexlet.code.data.JSONUtils;
 import hexlet.code.data.JacksonObjectMapperMakingHostAddresses;
 import hexlet.code.data.ReadFile;
 
+import java.util.Map;
+
 public class Differ {
     public static String generate(String filePath1, String filePath2) {
-        String contentFile1 = null;
-        try {
-            contentFile1 = ReadFile.read(filePath1);
-            //contentFile1 = ReadFile.read("testFile");//testFile
-           // System.out.println(contentFile1 + " test");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        var parseJson1 = JacksonObjectMapperMakingHostAddresses.parse(String.valueOf(contentFile1));
-        String contentFile2 = null;
-        try {
-            contentFile2 = ReadFile.read(filePath2);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        var parseJson2 = JacksonObjectMapperMakingHostAddresses.parse(contentFile2);
-        var comparedDatas = ComparingMaps.compare(parseJson1, parseJson2);
+        var parseFile1Content = parsingInit(filePath1);
+        var parseFile2Content = parsingInit(filePath2);
+        var comparedDatas = ComparingMaps.compare((Map) parseFile1Content, (Map) parseFile2Content);
         var checkDataForNull = CheckingForNull.check(comparedDatas);
-        var dataString = CreatingDataString.create(checkDataForNull);
-        return dataString;
+        return CreatingDataString.create(checkDataForNull);
+    }
+
+    private static Object parsingInit(String filePath1) {
+        String content;
+        try {
+            content = ReadFile.read(filePath1);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        boolean validJSON = JSONUtils.isJSONValid(content);
+        Map parseJson1;
+        if (validJSON) {
+            parseJson1 = JacksonObjectMapperMakingHostAddresses.parse(String.valueOf(content));
+        } else {
+            return "JSON file is no valid";
+        }
+        return parseJson1;
     }
 }
