3734165a78a635770fa8723a20690efc7b758496
Daukaevt <mupbam1@gmail.com>
2023-01-22 23:25:26 +0300

    typo fixed

--- hexlet.code/app/src/main/java/hexlet/code/Differ.java
+++ hexlet.code/app/src/main/java/hexlet/code/Differ.java
@@ -7,15 +7,21 @@ import hexlet.code.data.ReadFile;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2) {
+    public static String generate(
+            final String filePath1,
+            final String filePath2
+    ) {
         var parseFile1Content = parsingInit(filePath1);
         var parseFile2Content = parsingInit(filePath2);
-        var comparedDatas = ComparingMaps.compare((Map) parseFile1Content, (Map) parseFile2Content);
+        var comparedDatas = ComparingMaps.compare(
+                (Map) parseFile1Content,
+                (Map) parseFile2Content
+        );
         var checkDataForNull = CheckingForNull.check(comparedDatas);
         return CreatingDataString.create(checkDataForNull);
     }
 
-    private static Object parsingInit(String filePath1) {
+    private static Object parsingInit(final String filePath1) {
         String content;
         try {
             content = ReadFile.read(filePath1);
@@ -25,7 +31,8 @@ public class Differ {
         boolean validJSON = JSONUtils.isJSONValid(content);
         Map parseJson1;
         if (validJSON) {
-            parseJson1 = JacksonObjectMapperMakingHostAddresses.parse(String.valueOf(content));
+            parseJson1 = JacksonObjectMapperMakingHostAddresses
+                    .parse(String.valueOf(content));
         } else {
             return "JSON file is no valid";
         }
