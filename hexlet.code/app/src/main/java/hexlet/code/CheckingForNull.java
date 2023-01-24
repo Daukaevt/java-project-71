6ce7772aa858923029c854b1cef03b3840984a3e
Daukaevt <mupbam1@gmail.com>
2023-01-24 21:06:47 +0300

    checkstyle typo fixed

--- hexlet.code/app/src/main/java/hexlet/code/CheckingForNull.java
+++ hexlet.code/app/src/main/java/hexlet/code/CheckingForNull.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class CheckingForNull {
-    public static String[][] check(String[][] comparedDatas) {
+    public static String[][] check(final String[][] comparedDatas) {
         for (int j = 0; j < comparedDatas.length; j++) {
             for (int i = 0; i < comparedDatas[j].length; i++) {
                 if (comparedDatas[j][i] == null) {
