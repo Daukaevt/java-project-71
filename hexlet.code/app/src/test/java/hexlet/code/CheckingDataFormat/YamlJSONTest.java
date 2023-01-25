1647e2af19268da6c17a8b8d043a11c4d86ef3e4
Daukaevt <mupbam1@gmail.com>
2023-01-25 06:06:11 +0300

    test utils added

--- hexlet.code/app/src/test/java/hexlet/code/CheckingDataFormat/YamlJSONTest.java
+++ hexlet.code/app/src/test/java/hexlet/code/CheckingDataFormat/YamlJSONTest.java
@@ -1,11 +1,13 @@
 package hexlet.code.CheckingDataFormat;
 
 public class YamlJSONTest {
-    public String yamlContent = "---\n" +
+    String yamlContent = "---\n" +
             "  host: hexlet.io\n" +
             "  timeout: 50\n" +
             "  proxy: 123.234.53.22\n" +
             "  follow: false";
 
-    public String jsonContent = "{\"host\":\"hexlet.io\",\"timeout\":50,\"proxy\":\"123.234.53.22\",\"follow\":false}";
+    String jsonContent = "{\"host\":\"hexlet.io\",\"timeout\":50,\"proxy\":\"123.234.53.22\",\"follow\":false}";
+
+    String tmpYamlFilePath = "tmpFile1.yaml";
 }
