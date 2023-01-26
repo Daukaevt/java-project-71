1597b8514e5442e74bdabc6d9ca30fefe721d8cf
Daukaevt <mupbam1@gmail.com>
2023-01-26 15:32:33 +0300

    DataFormating.convertYamlToJson method call moved to parsingInit method && wrong format exception fixed

--- hexlet.code/app/src/main/java/hexlet/code/Differ.java
+++ hexlet.code/app/src/main/java/hexlet/code/Differ.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
-import hexlet.code.CheckingDataFormat.FileContentFormatToJSON;
+import hexlet.code.data.DataFormating;
 import hexlet.code.data.JSONUtils;
 import hexlet.code.data.JacksonObjectMapperMakingHostAddresses;
 import hexlet.code.data.ReadFile;
-import hexlet.code.utils.DeletingFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class Differ {
@@ -13,14 +13,8 @@ public class Differ {
             final String filePath1,
             final String filePath2
     ) {
-        var pathToJsonTempFile1
-                = FileContentFormatToJSON.factory(filePath1, "tmpFile1.yaml");
-        var pathToJsonTempFile2
-                = FileContentFormatToJSON.factory(filePath2, "tmpFile2.yaml");
-        var parseFile1Content = parsingInit(pathToJsonTempFile1); //(filePath1);
-        var parseFile2Content = parsingInit(pathToJsonTempFile2); //(filePath2);
-        DeletingFile.delete("tmpFile1.yaml");
-        DeletingFile.delete("tmpFile2.yaml");
+        var parseFile1Content = parsingInit(filePath1);
+        var parseFile2Content = parsingInit(filePath2);
         var comparedDatas = ComparingMaps.compare(
                 (Map) parseFile1Content,
                 (Map) parseFile2Content
@@ -28,21 +22,32 @@ public class Differ {
         var checkDataForNull = CheckingForNull.check(comparedDatas);
         return CreatingDataString.create(checkDataForNull);
     }
-
+    //move to new class and rename
     private static Object parsingInit(final String filePath) {
         String content;
+        // строка из файла
         try {
             content = ReadFile.read(filePath);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        // строка json или нет
         boolean validJSON = JSONUtils.isJSONValid(content);
-        Map parseJson1;
+        Map parseJson1 = null;
         if (validJSON) {
             parseJson1 = JacksonObjectMapperMakingHostAddresses
                     .parse(String.valueOf(content));
         } else {
-            return "JSON file is no valid";
+            //yaml или нет
+            String jsonContent = DataFormating.convertYamlToJson(content);
+            try {
+                parseJson1 = JacksonObjectMapperMakingHostAddresses
+                        .parse(jsonContent);
+            }  catch (Exception e) {
+                // все остальное
+                System.out.println("Wrong file format"); //+ e.getMessage());
+                System.exit(0);
+            }
         }
         return parseJson1;
     }
