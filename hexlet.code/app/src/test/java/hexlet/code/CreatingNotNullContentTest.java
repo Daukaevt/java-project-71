e86a539c0c5ac988bafc92deb09dea51ffd77138
Daukaevt <mupbam1@gmail.com>
2023-01-27 00:32:53 +0300

    test added

---
+++ hexlet.code/app/src/test/java/hexlet/code/CreatingNotNullContentTest.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import hexlet.code.data.DataFormating;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatingNotNullContentTest {
+    CreatingNotNullContent creatingNotNullContent;
+    Differ differ;
+
+    @BeforeEach
+    void setUp() {
+        creatingNotNullContent = new CreatingNotNullContent();
+        differ = new Differ();
+    }
+
+    @Test
+    void replaceNull() {
+        var testJson = differ.parsingInit("test.yaml");
+        assertEquals("{test=test, test1=null}", creatingNotNullContent.replaceNull(testJson).toString());
+    }
+}
