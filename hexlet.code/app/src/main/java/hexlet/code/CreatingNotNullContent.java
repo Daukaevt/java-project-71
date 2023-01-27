b2002f8ece3261db619730ce4f3c4dde2a8a56d6
Daukaevt <mupbam1@gmail.com>
2023-01-27 23:53:16 +0300

    Object moved to Map

--- hexlet.code/app/src/main/java/hexlet/code/CreatingNotNullContent.java
+++ hexlet.code/app/src/main/java/hexlet/code/CreatingNotNullContent.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CreatingNotNullContent {
-    public static Map replaceNull(Object parseFileContent) {
+    public static Map<String, Object> replaceNull(Map parseFileContent) {
         Map<String,Object> map = new HashMap<String, Object>((Map<? extends String, ?>) parseFileContent);
-        for (Object key: map.keySet()) {
+        for (String key: map.keySet()) {
             if (map.get(key) == null) {
                 map.put(String.valueOf(key), "null");
             }
