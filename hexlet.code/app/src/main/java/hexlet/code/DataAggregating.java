c83a6d30ee14b30fd5eaa3fe86da492e28d236dc
Daukaevt <mupbam1@gmail.com>
2023-01-30 07:38:31 +0300

    setValue method added

--- hexlet.code/app/src/main/java/hexlet/code/DataAggregating.java
+++ hexlet.code/app/src/main/java/hexlet/code/DataAggregating.java
@@ -3,44 +3,45 @@ package hexlet.code;
 import java.util.*;
 
 public class DataAggregating {
+    static String[][] agregator;
+
+    static int index;
     public static String[][] agregate(
             final Map parseJson1,
             final Map parseJson2
     ) {
         var size = parseJson1.size() + parseJson2.size();
-        String[][] agregator =
-                new String[2][size];
-        int index = 0;
+        agregator = new String[2][size];
+        index = 0;
         String temp;
-        for (Object key: parseJson1.keySet()) {
+        for (Object key : parseJson1.keySet()) {
             if (parseJson2.containsKey(key)) {
-                if (parseJson1.get(key) == parseJson2.get(key)) {
+                if (parseJson1.get(key).equals(parseJson2.get(key))
+                ) {
                     temp = "  " + key + ": " + parseJson1.get(key);
                 } else {
                     temp = "- " + key + ": " + parseJson1.get(key);
-                    agregator[0][index] = key.toString();
-                    agregator[1][index] = temp;
+                    setValue(index, temp, key);
                     index++;
                     temp = "+ " + key + ": " + parseJson2.get(key);
                 }
-                agregator[0][index] = key.toString();
-                agregator[1][index] = temp;
-                index++;
             } else {
                 temp = "- " + key + ": " + parseJson1.get(key);
-                agregator[0][index] = key.toString();
-                agregator[1][index] = temp;
-                index++;
             }
+            setValue(index, temp, key);
+            index++;
         }
-        for (Object key: parseJson2.keySet()) {
+        for (Object key : parseJson2.keySet()) {
             if (!parseJson1.containsKey(key)) {
                 temp = "+ " + key + ": " + parseJson2.get(key);
-                agregator[0][index] = key.toString();
-                agregator[1][index] = temp;
+                setValue(index, temp, key);
                 index++;
             }
         }
         return agregator;
+    }
+    private static void setValue(int index, String temp, Object key) {
+        agregator[0][index] = key.toString();
+        agregator[1][index] = temp;
     }
 }
