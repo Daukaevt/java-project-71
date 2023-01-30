dbe0a18efebd77eb75f71665c7ee1096b5439db8
Daukaevt <mupbam1@gmail.com>
2023-01-30 07:14:58 +0300

    createResult method added

---
+++ hexlet.code/app/src/main/java/hexlet/code/formatters/FormattingPlain.java
@@ -0,0 +1,48 @@
+package hexlet.code.formatters;
+
+import hexlet.code.MakingStyle;
+
+import java.util.ArrayList;
+
+public class FormattingPlain {
+    public static String format(String result) {
+        var tmpText = result.replaceAll("^.|.$", "");
+        var tmpLines = tmpText.lines();
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> tmpArrayList = new ArrayList<>();
+        tmpLines.forEach(arrayList::add);
+        arrayList.remove(0);
+        for (int i = 1; i < arrayList.size(); i++) {
+            String tmpSubstringPlus = null;
+            String line = arrayList.get(i);
+            var key = line.substring(2).replaceAll(":\s.*", "");
+            String firstValue = MakingStyle.makeStyle(line.replaceAll(".*:\s", ""));
+            String secondValue  = MakingStyle.makeStyle(arrayList.get(i + 1).replaceAll(".*:\s", ""));
+            switch (line.charAt(0)) {
+                case '-' -> {
+                    if (arrayList.get(i + 1).contains(key)) {
+                        String tempStrMinusPlus = "Property '" + key + "' was updated. From "
+                                + firstValue + " to " + secondValue ;
+                        tmpArrayList.add(tempStrMinusPlus);
+                        i++;
+                        } else {
+                        String tempStrMinus = "Property '" + key + "' was removed";
+                        tmpArrayList.add(tempStrMinus);
+                    }
+                }
+                case '+' -> {
+                    tmpSubstringPlus = "Property '" + key + "' was added with value: " + firstValue;
+                    tmpArrayList.add(tmpSubstringPlus);
+                }
+            }
+        }
+        return createResult(tmpArrayList);
+    }
+    private static String createResult(ArrayList<String> tmpArrayList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String line: tmpArrayList) {
+            stringBuilder.append(line).append("\n");
+        }
+        return stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "").toString();
+    }
+}
