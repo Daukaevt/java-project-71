00ef7bcad00e14dccedfadb6f72d4690cfaea806
Daukaevt <mupbam1@gmail.com>
2023-02-28 17:49:13 +0300

    makeStyle method call added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,53 +1,15 @@
 package hexlet.code;
 
-import hexlet.code.data.DataFormating;
-import hexlet.code.data.JSONUtils;
-import hexlet.code.data.ReadFile;
 
 import java.util.Map;
 
 public class Differ {
     public static String generate(
-            String filePath1, String filePath2, String stylish) throws Exception {
-        var parseFile1Content = parsingInit(filePath1);
-        Map file1Content = CreatingNotNullContent.replaceNull(parseFile1Content);
-        var parseFile2Content = parsingInit(filePath2);
-        Map file2Content = CreatingNotNullContent.replaceNull(parseFile2Content);
-        var comparedDatas = DataAggregating
-                .agregate(file1Content, file2Content);
-        var checkDataForNull = CheckingForNull.check(comparedDatas);
-        var result = SortingDataString.sort(checkDataForNull);
-     return result;
-    }
-    //move to new class and rename
-    public static Map parsingInit(final String filePath) {
-        String content;
-        // строка из файла
-        try {
-            content = ReadFile.read(filePath);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        // строка json или нет
-        boolean validJSON = JSONUtils.isJSONValid(content);
-        Map parseJson1 = null;
-        if (validJSON) {
-
-            parseJson1 = hexlet.code.JacksonObjectMapperMakingHostAddresses
-                    .getData(String.valueOf(content));
-                    //JacksonObjectMapperMakingHostAddresses
-                    //.parse(String.valueOf(content));
-        } else {
-            //yaml или нет
-            String jsonContent = DataFormating.convertYamlToJson(content);
-            try {
-                parseJson1 = JacksonObjectMapperMakingHostAddresses.getData(String.valueOf(content));
-            }  catch (Exception e) {
-                // все остальное
-                System.out.println("Wrong file format"); //+ e.getMessage());
-                System.exit(0);
-            }
-        }
-        return parseJson1;
+            String filePath1, String filePath2, String format) throws Exception {
+            Map parseJson1 = Reader.check(filePath1);
+            Map parseJson2 = Reader.check(filePath2);
+            String content = Parser.agregate(parseJson1, parseJson2);
+            content = ContentFormatter.makeStyle(content, format);
+     return content;
     }
 }
