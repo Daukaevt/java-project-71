45df10906f342dc2e9b52e07595e8fa6cd861183
Daukaevt <mupbam1@gmail.com>
2023-02-28 17:43:42 +0300

    separator rebuild

--- app/src/main/java/hexlet/code/formatters/FormattingJson.java
+++ app/src/main/java/hexlet/code/formatters/FormattingJson.java
@@ -1,31 +1,27 @@
 package hexlet.code.formatters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Splitter;
+import org.json.simple.JSONObject;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class FormattingJson {
     public static String format(String result) {
-        String jsonResult;
+        JSONObject jsonResult;
         String cutResult = result
-                .replaceAll("^.|.$", "")
-                .replaceAll("\n", "`");
-        String trimedRes = cutResult.replaceAll("^.|.$", "");
-        Map<String, String> properties = Splitter.on("`")
-                .withKeyValueSeparator(":")
-                .split(trimedRes);
-        try {
-            jsonResult = new ObjectMapper().writeValueAsString(properties);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+                .replaceAll("^.\n|..$", "");
+        String trimedRes = cutResult.replaceAll(".$", "");
+        if (trimedRes.endsWith("\n")) {
+            trimedRes = trimedRes.substring(0, trimedRes.length()-1);
         }
-        StringBuilder stringBuilder = new StringBuilder(jsonResult);
+        Map<String, String> properties = Splitter.on("\n")
+                .withKeyValueSeparator(": ")
+                .split(trimedRes);
+        jsonResult = new JSONObject(properties);
+        StringBuilder stringBuilder = new StringBuilder(String.valueOf(jsonResult));
         stringBuilder.insert(1, "\n")
-                .insert(jsonResult.length(), "\n");
-        String str = stringBuilder.toString().replaceAll("\",", "\",\n");
+                .insert(String.valueOf(jsonResult).length(), "\n");
+        String str = stringBuilder.toString().replaceAll("[^\\\\]\",", "\",\n");
         return str;
     }
 }
