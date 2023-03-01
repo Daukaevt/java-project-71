86e773768689017c337a410a585721af0187bf00
Daukaevt <mupbam1@gmail.com>
2023-03-01 11:55:19 +0300

    generate method stylish value added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,6 +4,9 @@ package hexlet.code;
 import java.util.Map;
 
 public class Differ {
+    public static String generate (String firstFilePath, String secondFilePath) throws Exception {
+        return Differ.generate(firstFilePath, secondFilePath, "stylish");
+    }
     public static String generate(
             String filePath1, String filePath2, String format) throws Exception {
             Map parseJson1 = Reader.check(filePath1);
