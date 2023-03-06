ba8ddd16efb83ef5bd8889855f9b0a89669c9ea7
Daukaevt <mupbam1@gmail.com>
2023-03-06 19:27:39 +0300

    regex added

--- app/src/main/java/hexlet/code/formatters/FormattingJson.java
+++ app/src/main/java/hexlet/code/formatters/FormattingJson.java
@@ -3,6 +3,7 @@ package hexlet.code.formatters;
 import com.google.common.base.Splitter;
 import org.json.simple.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class FormattingJson {
@@ -17,11 +18,18 @@ public class FormattingJson {
         Map<String, String> properties = Splitter.on("\n")
                 .withKeyValueSeparator(": ")
                 .split(trimedRes);
-        jsonResult = new JSONObject(properties);
+        HashMap hashMap = new HashMap();
+        for (String key:properties.keySet()) {
+            hashMap.put(key.replaceAll("^.{2}", ""), properties.get(key));
+        }
+        jsonResult = new JSONObject(hashMap);
         StringBuilder stringBuilder = new StringBuilder(String.valueOf(jsonResult));
         stringBuilder.insert(1, "\n")
                 .insert(String.valueOf(jsonResult).length(), "\n");
-        String str = stringBuilder.toString().replaceAll("[^\\\\]\",", "\",\n");
+        String str = stringBuilder.toString()
+                .replaceFirst("\n", "\n  ")
+                .replaceAll(".\",", "\",\n  ")
+                .replaceAll("\":\"[\\[|\\{]", "\":\"");
         return str;
     }
 }
