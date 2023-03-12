64b20b1e53909431737e0d0f5c2af82b271b9a45
Daukaevt <mupbam1@gmail.com>
2023-03-12 16:34:10 +0300

    read-parse-formate logic line added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,15 +4,19 @@ package hexlet.code;
 import java.util.Map;
 
 public class Differ {
-    public static String generate (String firstFilePath, String secondFilePath) throws Exception {
-        return Differ.generate(firstFilePath, secondFilePath, "stylish");
+    public static String generate(
+            final String firstFilePath,
+            final String secondFilePath
+    ) throws Exception {
+        return Differ.generate(
+                firstFilePath, secondFilePath, "stylish");
     }
     public static String generate(
-            String filePath1, String filePath2, String format) throws Exception {
-            Map parseJson1 = Reader.check(filePath1);
-            Map parseJson2 = Reader.check(filePath2);
-            String content = Parser.agregate(parseJson1, parseJson2);
-            content = ContentFormatter.makeStyle(content, format);
-     return content;
+            final String filePath1, final String filePath2, final String format
+    ) throws Exception {
+        Map<String, String> parseJson1 = Parser.parse(Reader.readFile(filePath1));
+        Map<String, String> parseJson2 = Parser.parse(Reader.readFile(filePath2));
+        String content = Parser.agregate(parseJson1, parseJson2);
+     return ContentFormatter.makeStyle(content, format);
     }
 }
