7ee036524fdebbdbeced14989756c12c4a3c6c83
Daukaevt <mupbam1@gmail.com>
2023-03-12 16:34:51 +0300

    some parse methods added

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,15 +1,144 @@
 package hexlet.code;
 
-import java.util.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Parser {
-    static String[][] agregator;
-    static int index;
+    public static Map parse(final String content) {
+        boolean validJSON = isJSONValid(content);
+        Map parseJsonToMap = null;
+        if (validJSON) {
+            parseJsonToMap = getData(String.valueOf(content));
+        } else {
+            String jsonContent = convertYamlToJson(content);
+            try {
+                parseJsonToMap = getData(jsonContent);
+            }  catch (Exception e) {
+                System.out.println("Wrong file format");
+                System.exit(0);
+            }
+        }
+        return replace(parseJsonToMap);
+    }
+    public static Map<String, Object> replace(
+            final Map parseFileContent) {
+        Map<String, Object> map =
+                new HashMap<>((Map<? extends String, ?>) parseFileContent);
+        HashMap hashMap = new HashMap();
+        for (String key: map.keySet()) {
+            String value;
+            map.putIfAbsent(key, "null");
+            value = map.get(key).toString();
+            if (value == null) {
+                hashMap.put(key, "null");
+            } else if (value.startsWith("[")) {
+                value = value.replaceAll("^\\[", "")
+                        .replaceAll("\\]$", "");
+                Stream<String> stream = Arrays.stream(value.split(","));
+                List<String> list = stream.collect(Collectors.toList());
+                list.replaceAll(s -> s.replaceAll("^\"", "")
+                        .replaceAll("\"$", ""));
+                hashMap.put(key, list);
+            } else if (value.startsWith("{")) {
+                Map newMap = new HashMap();
+                for (Object keyObj: parseFileContent.keySet()) {
+                    String keyStr = keyObj.toString().replaceAll("^\"", "")
+                            .replaceAll("\"$", "");
+                    String valueStr = parseFileContent
+                            .get(keyObj).toString().replaceAll("^\"", "")
+                            .replaceAll("\"$", "");
+                    newMap.put(keyStr, valueStr);
+                }
+                hashMap.put(key, newMap);
+            } else {
+                hashMap.put(key, map.get(key));
+            }
+        }
+        return hashMap;
+    }
+    public static Map getData(final String contentFile1) {
+        if (contentFile1.startsWith("[")) {
+            JSONArray jsonArray;
+            JSONParser jsonParser = new JSONParser();
+            try {
+                jsonArray = (JSONArray) jsonParser.parse(contentFile1);
+                HashMap hashMap = new HashMap();
+                for (Object o : jsonArray) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        Map<String, String> map = mapper
+                                .readValue(String.valueOf(o), Map.class);
+                        for (Object key : map.keySet()) {
+                            hashMap.put(key, map.get(key));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return hashMap;
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return getJsonObject(contentFile1);
+        }
+    }
+    private static Map getJsonObject(final String contentFile1) {
+        JSONObject jsonObject;
+        JSONParser jsonParser = new JSONParser();
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(contentFile1);
+            return jsonObject;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static boolean isJSONValid(final String jsonInString) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.readTree(jsonInString);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    public static String convertYamlToJson(final String yaml) {
+        ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+        Object obj;
+        try {
+            obj = yamlReader.readValue(yaml, Object.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        ObjectMapper jsonWriter = new ObjectMapper();
+        try {
+            return jsonWriter.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static String[][] agregator;
+
     public static String agregate(
-            Map parseJson1, Map parseJson2) {
+            final Map parseJson1, final Map parseJson2) {
         var size = parseJson1.size() + parseJson2.size();
         agregator = new String[2][size];
-        index = 0;
+        int index = 0;
         String temp;
         for (Object key : parseJson1.keySet()) {
             if (parseJson2.containsKey(key)) {
@@ -37,8 +166,9 @@ public class Parser {
         }
         return sort(checkForNull(agregator));
     }
-
-    private static void setValue(int index, String temp, Object key) {
+    private static void setValue(
+            final int index, final String temp, final Object key
+    ) {
         agregator[0][index] = key.toString();
         agregator[1][index] = temp;
     }
@@ -52,7 +182,6 @@ public class Parser {
         }
         return comparedDatas;
     }
-
     public static String sort(final String[][] comparedData) {
         var tempArr = Arrays.stream(comparedData[0]).sorted().toArray();
         StringBuilder stringBuilder = new StringBuilder();
