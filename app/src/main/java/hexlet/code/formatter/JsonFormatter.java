73768358aef7c9a7fdc3069e4e020efe5f64b798
Daukaevt <mupbam1@gmail.com>
2023-04-12 18:42:20 +0300

    jsonFormat method added

---
+++ app/src/main/java/hexlet/code/formatter/JsonFormatter.java
@@ -0,0 +1,38 @@
+package hexlet.code.formatter;
+
+import hexlet.code.utils.Wrapper;
+
+import java.util.TreeMap;
+
+public class JsonFormatter {
+    public static String jsonFormat(TreeMap<String, Wrapper> unitMap) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{\n");
+        for (Object key : unitMap.keySet()) {
+            Wrapper values = unitMap.get(key);
+            String value1 = values.getValue1();
+            String tempPlus = "  \"+ " + key + "\": \""
+                    + value1.replaceAll("\"", "")
+                    .replaceAll(",\\S", ",\s") + "\"\n";
+            String value2 = values.getValue2();
+            String tempMinus = "  \"- " + key + "\": \""
+                    + value2.replaceAll("\"", "")
+                    .replaceAll(",\\S", ",\s") + "\"\n";
+            if (value1.equals("-absent-")) {
+                stringBuilder.append(tempMinus);
+                continue;
+            }
+            if (value2.equals("-absent-")) {
+                stringBuilder.append(tempPlus);
+                continue;
+            }
+            if (value1.equals(value2)) {
+                stringBuilder.append(tempMinus.replaceAll("\s{2}\"- ", "  \"  "));
+            } else {
+                stringBuilder.append(tempMinus);
+                stringBuilder.append(tempPlus);
+            }
+        }
+        return stringBuilder + "}";
+    }
+}
