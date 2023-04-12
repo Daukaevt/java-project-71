7c70cbaeaa3e50c3ca45b63cc5adf0a57d0181d0
Daukaevt <mupbam1@gmail.com>
2023-04-12 18:48:03 +0300

    unite metrhod added

---
+++ app/src/main/java/hexlet/code/Uniter.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import hexlet.code.utils.Wrapper;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Uniter {
+    public static TreeMap unite (Map<String, Object> parseJson1, Map<String, Object> parseJson2){
+        TreeMap<String, Wrapper> unitMap = new TreeMap<>();
+        for (Object key: parseJson1.keySet()) {
+            if (parseJson2.containsKey(key)) {
+                unitMap.put(key.toString(), new Wrapper(
+                        (parseJson1.get(key).toString()),
+                        (parseJson2.get(key).toString())));
+            } else {
+                unitMap.put(key.toString(), new Wrapper(
+                        (parseJson1.get(key).toString()),
+                        ("-absent-")
+                ));
+            }
+        }
+        for (Object key: parseJson2.keySet()) {
+            if (!parseJson1.containsKey(key)) {
+                unitMap.put(key.toString(), new Wrapper(
+                        ("-absent-"),
+                        (parseJson2.get(key).toString())
+                ));
+            }
+        }
+        return unitMap;
+    }
+}
