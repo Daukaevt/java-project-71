1149d26ae9df76710ca78162026eff2ac5e6e7fb
Daukaevt <mupbam1@gmail.com>
2023-04-16 06:36:22 +0300

    checkstyle typo fixed

--- app/src/main/java/hexlet/code/formatter/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatter/PlainFormatter.java
@@ -8,34 +8,40 @@ import java.util.TreeMap;
 import static java.lang.Integer.parseInt;
 
 public class PlainFormatter {
-    public static String plainFormate(TreeMap<String, Wrapper> unitMap) {
+    public static String plainFormate(final TreeMap<String, Wrapper> unitMap) {
         StringBuilder stringBuilder = new StringBuilder();
         for (Object key: unitMap.keySet()) {
             Wrapper values = unitMap.get(key);
             String value1 = values.getValue1();
             String value2 = values.getValue2();
             if (value1.equals("-absent-")) {
-                stringBuilder.append("Property '" + key + "' was added with value: " + getSingleQuotes3(value2) + "\n");
+                stringBuilder.append("Property '" + key
+                        + "' was added with value: "
+                        + getSingleQuotes3(value2) + "\n");
                 continue;
             }
             if (value2.equals("-absent-")) {
-                stringBuilder.append("Property '" + key + "' was removed" + "\n");
+                stringBuilder.append("Property '"
+                        + key + "' was removed" + "\n");
                 continue;
             }
             if (!value1.equals(value2)) {
-                stringBuilder.append("Property '" + key + "' was updated. From " + getSingleQuotes3(value1)
+                stringBuilder.append("Property '" + key
+                        + "' was updated. From " + getSingleQuotes3(value1)
                         + " to " + getSingleQuotes3(value2) + "\n");
             }
         }
-        return stringBuilder.toString().replaceAll("\n$","");
+        return stringBuilder.toString().replaceAll("\n$", "");
     }
-    public static String getSingleQuotes3(String value) {
+    public static String getSingleQuotes3(final String value) {
+        String singleQuotesEnclosedValue = null;
         if (!isBoolean(value)
                 && !isNumeric(value)
                 && !value.equals("null")) {
-            value = "'" + value + "'";
+            singleQuotesEnclosedValue = "'" + value + "'";
         }
-        return value.replaceAll("'\\[.*]'|'\\{.*}'", "[complex value]");
+        return singleQuotesEnclosedValue
+                .replaceAll("'\\[.*]'|'\\{.*}'", "[complex value]");
     }
     public static boolean isNumeric(final String str) {
         try {
