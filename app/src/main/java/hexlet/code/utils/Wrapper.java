2f3610f41132ef09608980764483c54208761337
Daukaevt <mupbam1@gmail.com>
2023-04-16 06:37:05 +0300

    checkstyle typo fixed

--- app/src/main/java/hexlet/code/utils/Wrapper.java
+++ app/src/main/java/hexlet/code/utils/Wrapper.java
@@ -1,15 +1,17 @@
 package hexlet.code.utils;
 
 public class Wrapper {
-    public Wrapper(String value1, String value2) {
+    public Wrapper(final String value1, final String value2) {
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    public String getValue1() { return this.value1; }
-    public String getValue2() { return this.value2; }
+    public String getValue1() {
+        return this.value1; }
+    public String getValue2() {
+        return this.value2; }
 
-    private String value1;
-    private String value2;
+    private final String value1;
+    private final String value2;
 
 }
