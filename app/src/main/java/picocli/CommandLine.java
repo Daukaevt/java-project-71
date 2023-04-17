a866cc0725f556cf79d2b2ab91fa965c1b593f5e
Daukaevt <mupbam1@gmail.com>
2023-04-17 06:18:15 +0300

    chekstyle typo fixed

--- app/src/main/java/picocli/CommandLine.java
+++ app/src/main/java/picocli/CommandLine.java
@@ -34094,8 +34094,8 @@ InitialValueState.CACHED;*/
             if (arity.min > 0) {
                 args.push(quotedValue);
                 boolean discontinue = assertNoMissingMandatoryParameter(
-                argSpec, args, 0, arity) || isArgResemblesOptionThereforeDiscontinue(
-                argSpec, args, 0, arity); args.pop();
+                    argSpec, args, 0, arity) || isArgResemblesOptionThereforeDiscontinue(
+                    argSpec, args, 0, arity); args.pop();
                 if (discontinue) {
                     return 0;
                 }
