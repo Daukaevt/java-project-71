16cbf7971fa6bf0e2203865e2275177fb4ac4c5f
Daukaevt <mupbam1@gmail.com>
2023-04-24 19:02:28 +0300

    checkstyle typo fixed_1

--- app/src/main/java/picocli/CommandLine.java
+++ app/src/main/java/picocli/CommandLine.java
@@ -29048,7 +29048,7 @@ InitialValueState.CACHED;*/
          * "",
          * param.description()[i]}}.</p>
          */
-        final private static class DefaultParameterRenderer implements IParameterRenderer {
+        final public static class DefaultParameterRenderer implements IParameterRenderer {
             private final String requiredMarker;
             private final boolean showDefaultValues;
 
@@ -29078,9 +29078,7 @@ InitialValueState.CACHED;*/
                 String[] description = param.description();
                 Text[] descriptionFirstLines = createDescriptionFirstLines(
                         scheme, param, description, showDefault);
-                result.add(new Text[]{
-                        requiredParameter, empty, empty, label, descriptionFirstLines[0]
-                });
+                result.add(new Text[]{requiredParameter, empty, empty, label, descriptionFirstLines[0]});
                 for (int i = 1; i < descriptionFirstLines.length; i++) {
                     result.add(new Text[]{empty, empty, empty, empty,
                             descriptionFirstLines[i]});
@@ -29090,7 +29088,7 @@ InitialValueState.CACHED;*/
                             scheme.ansi(
                             ).new Text(description[i], scheme).splitLines();
                     for (Text line : descriptionNextLines) {
-                        result.add(new Text[]{empty,empty, empty, empty, line});
+                        result.add(new Text[]{empty, empty, empty, empty, line});
                     }
                 }
                 if (showDefault[0]) {
@@ -29112,7 +29110,7 @@ InitialValueState.CACHED;*/
          * "...") to indicate that any number of values is allowed
          * for options or parameters with variable arity.
          */
-        final private static class DefaultParamLabelRenderer implements IParamLabelRenderer {
+        final public static class DefaultParamLabelRenderer implements IParamLabelRenderer {
             private final CommandSpec commandSpec;
 
             /**
