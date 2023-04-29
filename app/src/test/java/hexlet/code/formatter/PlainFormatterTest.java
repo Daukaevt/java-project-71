de3145d8ba6ea239fca8fe1900ae0af245165f56
Daukaevt <mupbam1@gmail.com>
2023-04-29 15:08:43 +0300

    plainFormate method added

---
+++ app/src/test/java/hexlet/code/formatter/PlainFormatterTest.java
@@ -0,0 +1,41 @@
+package hexlet.code.formatter;
+
+import hexlet.code.utils.Wrapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlainFormatterTest {
+    TreeMap<String, Wrapper> testMap;
+    String expected;
+
+    @BeforeEach
+    void setUp() {
+        testMap = new TreeMap<>();
+        testMap.put("testKey", new Wrapper("value1", "value2"));
+        expected = "Property 'testKey' was updated. From 'value1' to 'value2'";
+    }
+
+    @Test
+    void plainFormate() {
+        assertEquals(expected, PlainFormatter.plainFormate(testMap));
+    }
+
+    @Test
+    void getSingleQuotes3() {
+        assertEquals("'test'", PlainFormatter.getSingleQuotes3("test"));
+    }
+
+    @Test
+    void isNumeric() {
+        assertTrue(PlainFormatter.isNumeric("2"));
+    }
+
+    @Test
+    void isBoolean() {
+        assertTrue(PlainFormatter.isBoolean("false"));
+    }
+}
