2d5dfb60df4ee867a7a777954fac7fbddce315e9
Daukaevt <mupbam1@gmail.com>
2023-04-29 15:16:41 +0300

    checkstyle typo fixed

--- app/src/main/java/hexlet/code/formatter/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatter/StylishFormatter.java
@@ -8,25 +8,45 @@ public class StylishFormatter {
     public static String stylishFormat(final TreeMap<String, Wrapper> unitMap) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("{\n");
-        for (Object key: unitMap.keySet()) {
+        unitMap.keySet().forEach(key -> {
             Wrapper values = unitMap.get(key);
             String value1 = values.getValue1();
             String value2 = values.getValue2();
             if (value1.equals("-absent-")) {
-                stringBuilder.append("  + " + key + ": " + value2 + "\n");
-                continue;
+                stringBuilder.append("  + ")
+                        .append(key)
+                        .append(": ")
+                        .append(value2)
+                        .append("\n");
+                return;
             }
             if (value2.equals("-absent-")) {
-                stringBuilder.append("  - " + key + ": " + value1 + "\n");
-                continue;
+                stringBuilder.append("  - ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
+                return;
             }
             if (!value1.equals(value2)) {
-                stringBuilder.append("  - " + key + ": " + value1 + "\n");
-                stringBuilder.append("  + " + key + ": " + value2 + "\n");
+                stringBuilder.append("  - ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
+                stringBuilder.append("  + ")
+                        .append(key)
+                        .append(": ")
+                        .append(value2)
+                        .append("\n");
             } else {
-                stringBuilder.append("    " + key + ": " + value1 + "\n");
+                stringBuilder.append("    ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
             }
-        }
+        });
         return stringBuilder + "}";
     }
 }
