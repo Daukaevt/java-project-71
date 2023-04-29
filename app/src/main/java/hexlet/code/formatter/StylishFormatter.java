e58dbf03fd03305895da804fc59222a5e29a1566
Daukaevt <mupbam1@gmail.com>
2023-04-29 19:14:31 +0300

    codeclimate typo fixed

--- app/src/main/java/hexlet/code/formatter/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatter/StylishFormatter.java
@@ -7,20 +7,16 @@ import java.util.TreeMap;
 public class StylishFormatter {
     public static String stylishFormat(final TreeMap<String, Wrapper> unitMap) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{\n");
         unitMap.keySet().forEach(key -> {
-            Wrapper values = unitMap.get(key);
-            String value1 = values.getValue1();
-            String value2 = values.getValue2();
-            if (value1.equals("-absent-")) {
+            String value1 = unitMap.get(key).getValue1();
+            String value2 = unitMap.get(key).getValue2();
+            if ("-absent-".equals(value1)) {
                 stringBuilder.append("  + ").append(key).append(": ")
-                        .append(value2).append("\n");
-                return;
+                        .append(value2).append("\n"); return;
             }
-            if (value2.equals("-absent-")) {
+            if ("-absent-".equals(value2)) {
                 stringBuilder.append("  - ").append(key).append(": ")
-                        .append(value1).append("\n");
-                return;
+                        .append(value1).append("\n"); return;
             }
             if (!value1.equals(value2)) {
                 stringBuilder.append("  - ").append(key).append(": ")
@@ -32,6 +28,6 @@ public class StylishFormatter {
                         .append(value1).append("\n");
             }
         });
-        return stringBuilder + "}";
+        return "{\n" + stringBuilder + "}";
     }
 }
