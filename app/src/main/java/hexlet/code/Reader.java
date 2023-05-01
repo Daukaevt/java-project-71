53b936dbd2388c5e2e5a57f9bbb0e46c0eed768d
Daukaevt <mupbam1@gmail.com>
2023-05-01 18:16:22 +0300

    isExist method remooved

--- app/src/main/java/hexlet/code/Reader.java
+++ app/src/main/java/hexlet/code/Reader.java
@@ -4,23 +4,12 @@ package hexlet.code;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Reader {
     public static String readFile(final String filePath) {
         String content = null;
-        Path path = Paths.get(filePath).toAbsolutePath().normalize();
-        if (!Files.exists(path)) {
-            try {
-                throw new Exception("File '" + path + "' does not exist");
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
         try (BufferedReader br =
-                     new BufferedReader(new FileReader(String.valueOf(path)))) {
+                     new BufferedReader(new FileReader(String.valueOf(filePath)))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
             while (line != null) {
