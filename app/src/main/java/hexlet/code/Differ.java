8b415e370893913d501b1b571db4402244703c01
Daukaevt <mupbam1@gmail.com>
2023-05-01 18:17:31 +0300

    if file exist block added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,6 +4,7 @@ package hexlet.code;
 import hexlet.code.formatter.ContentFormatter;
 import hexlet.code.utils.Wrapper;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -21,9 +22,13 @@ public class Differ {
             final String filePath1, final String filePath2, final String format
     ) throws Exception {
         Path path1 = Paths.get(filePath1).normalize();
+        Path path2 = Paths.get(filePath2).normalize();
+        if (!Files.exists(path1) || !Files.exists(path2)) {
+            return "File does not exist";
+        }
         Map<String, Object> parseJson1 =
                 Parser.parse(Reader.readFile(path1.toAbsolutePath().toString()));
-        Path path2 = Paths.get(filePath2).normalize();
+
         Map<String, Object> parseJson2 =
                 Parser.parse(Reader.readFile(path2.toAbsolutePath().toString()));
         TreeMap<String, Wrapper> unitMap = Uniter.unite(parseJson1, parseJson2);
