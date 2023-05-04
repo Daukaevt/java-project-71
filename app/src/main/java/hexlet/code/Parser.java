a5e1265f206cc235b6fb61def3723647a8479725
Daukaevt <mupbam1@gmail.com>
2023-05-04 21:17:32 +0300

    throws Exception conditions added

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -20,21 +20,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Parser {
-    public static Map parse(final String content) {
+    public static Map parse(final String content) throws JsonProcessingException, ParseException {
         if (content.isEmpty()) {
             return new HashMap<String, String>();
         }
         boolean validJSON = isJSONValid(content);
-        Map parseJsonToMap = null;
+        Map parseJsonToMap;
         if (validJSON) {
             parseJsonToMap = getJson(content);
         } else {
-            String jsonContent = convertYamlToJsonData(content);
             try {
-                parseJsonToMap = getJson(jsonContent);
+                parseJsonToMap = getJson(convertYamlToJsonData(content));
             } catch (Exception e) {
-                System.out.println("Wrong file format");
-                System.exit(0);
+                HashMap<Object, Object> unstructuredData = new HashMap<>();
+                unstructuredData.put("newKey", content);
+                return unstructuredData;
             }
         }
         return replaceNull(parseJsonToMap);
@@ -49,62 +49,53 @@ public class Parser {
             return false;
         }
     }
-    private static Map getJson(String content) {
+
+    public static Map getJson(String content) throws JsonProcessingException, ParseException {
         return content.startsWith("[")
-                ? getJsonArray(content)
-                : getJsonObject(content);
+                ? parseJsonArray(content)
+                : parseJsonObject(content);
     }
 
-    public static Map getJsonArray(final String contentFile1) {
+    public static Map parseJsonArray(final String contentFile1) throws JsonProcessingException {
         JSONArray jsonArray;
         JSONParser jsonParser = new JSONParser();
         try {
             jsonArray = (JSONArray) jsonParser.parse(contentFile1);
-            HashMap hashMap = new HashMap();
-            for (Object o : jsonArray) {
-                ObjectMapper mapper = new ObjectMapper();
-                try {
-                    Map<String, String> map = mapper
-                            .readValue(String.valueOf(o), Map.class);
-                        for (Object key : map.keySet()) {
-                            hashMap.put(key, map.get(key));
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return hashMap;
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-    }
-    public static Map getJsonObject(final String contentFile1) {
-        JSONObject jsonObject;
-        JSONParser jsonParser = new JSONParser();
-        try {
-            jsonObject = (JSONObject) jsonParser.parse(contentFile1);
-            return jsonObject;
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+        return getArrayDataMap(jsonArray);
     }
-    public static String convertYamlToJsonData(final String yaml) {
+
+    public static Map getArrayDataMap(JSONArray jsonArray) throws JsonProcessingException {
+        HashMap dataMap = new HashMap();
+        for (Object o : jsonArray) {
+            ObjectMapper mapper = new ObjectMapper();
+            Map contentMapping;
+                contentMapping = mapper.readValue(String.valueOf(o), Map.class);
+            for (Object key : contentMapping.keySet()) {
+                dataMap.put(key, contentMapping.get(key));
+            }
+        }
+        return dataMap;
+    }
+
+    public static Map parseJsonObject(final String contentFile1) throws ParseException {
+        JSONObject jsonObject;
+        JSONParser jsonParser = new JSONParser();
+            jsonObject = (JSONObject) jsonParser.parse(contentFile1);
+            return jsonObject;
+    }
+    public static String convertYamlToJsonData(final String yaml) throws JsonProcessingException {
         ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
         Object obj;
-        try {
             obj = yamlReader.readValue(yaml, Object.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
         ObjectMapper jsonWriter = new ObjectMapper();
-        try {
             return jsonWriter.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
-    public static Map<String, String> replaceNull(
-            final Map parseFileContent) {
+
+    public static HashMap replaceNull(
+            final Map parseFileContent) throws JsonProcessingException {
         HashMap hashMap = new HashMap();
         Pattern braces = Pattern.compile("\\{.*}");
         Pattern squareBrackets = Pattern.compile("\\[.*]");
@@ -114,11 +105,9 @@ public class Parser {
             value = parseFileContent.get(key).toString();
             Matcher hasBraces = braces.matcher(value);
             Matcher hasSquareBrackets = squareBrackets.matcher(value);
-            if (value == null) {
-                hashMap.put(key, "null");
-            } else if (hasSquareBrackets.find()) {
+            if (hasSquareBrackets.find()) {
                 value = value.replaceAll("^\\[", "")
-                        .replaceAll("\\]$", "");
+                        .replaceAll("]$", "");
                 Stream<String> stream = Arrays.stream(value.split(","));
                 List<String> list = stream.collect(Collectors.toList());
                 list.replaceAll(s -> s.replaceAll("\"", "")
@@ -126,13 +115,8 @@ public class Parser {
                 hashMap.put(key, list);
             } else if (hasBraces.find()) {
                 ObjectMapper mapper = new ObjectMapper();
-                try {
-                    Map<String, String> map = mapper
-                            .readValue(value, Map.class);
+                    Map map = mapper.readValue(value, Map.class);
                     hashMap.put(key, map);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             } else {
                 hashMap.put(key, parseFileContent.get(key));
             }
