19ebebf2411b021c874ca97f37bd06182b9b10fa
Daukaevt <mupbam1@gmail.com>
2023-05-05 12:10:21 +0300

    checkstyle typo fixed

--- app/src/test/java/hexlet/code/ReaderTest.java
+++ app/src/test/java/hexlet/code/ReaderTest.java
@@ -8,33 +8,30 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ReaderTest {
 
-    final private String testFilePath1
+    private final String testFilePath1
             = "/home/timur/IdeaProjects/java-project-71/app/src/test/resources/nestedFile1.json";
-    String fileContent = Reader.readFile(testFilePath1);
-    String expected;
+    private final String fileContent = Reader.readFile(testFilePath1);
 
     @Test
     void readFile() {
-        expected = """
-            {
-              "posts": [
+        String expected = """
                 {
-                  "id": 1,
-                  "title": "hello"
-                }
-              ],
-              "profile": {
-                "name": "typicode"
-              },
-              "field": null
-            }""";
+                  "posts": [
+                    {
+                      "id": 1,
+                      "title": "hello"
+                    }
+                  ],
+                  "profile": {
+                    "name": "typicode"
+                  },
+                  "field": null
+                }""";
         assertEquals(expected, fileContent);
     }
 
     @Test
     void readFileThrowException() {
-        assertThrows(RuntimeException.class, () -> {
-            Reader.readFile("wrongPath");
-        });
+        assertThrows(RuntimeException.class, () -> Reader.readFile("wrongPath"));
     }
 }
