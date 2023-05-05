b492e143860a9d6368413ae823344839c75a758b
Daukaevt <mupbam1@gmail.com>
2023-05-05 11:38:58 +0300

    checkstyle typo fixed

--- app/src/test/java/hexlet/code/formatter/StylishFormatterTest.java
+++ app/src/test/java/hexlet/code/formatter/StylishFormatterTest.java
@@ -1,6 +1,7 @@
 package hexlet.code.formatter;
 
 import hexlet.code.utils.Wrapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.TreeMap;
@@ -8,15 +9,34 @@ import java.util.TreeMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StylishFormatterTest {
+    private TreeMap<String, Wrapper> testMap;
+    private String expected;
+    @BeforeEach
+    void setUp() {
+        testMap = new TreeMap<>();
+    }
 
     @Test
     void stylishFormat() {
-        TreeMap<String, Wrapper> testMap = new TreeMap<>();
         testMap.put("testKey", new Wrapper("value1", "value2"));
-        String expected = """
+         expected = """
                 {
                   - testKey: value1
                   + testKey: value2
+                }""";
+        assertEquals(expected, StylishFormatter.stylishFormat(testMap));
+    }
+
+    @Test
+    void ifValue1Absent() {
+        testMap.put("testKey", new Wrapper("-absent-", "value2"));
+        testMap.put("testKey2", new Wrapper("value1", "-absent-"));
+        testMap.put("testKey3", new Wrapper("value3", "value3"));
+        expected = """
+                {
+                  + testKey: value2
+                  - testKey2: value1
+                    testKey3: value3
                 }""";
         assertEquals(expected, StylishFormatter.stylishFormat(testMap));
     }
