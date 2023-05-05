b47518e7e2497ab61ef1e62517eaf84124c21ef7
Daukaevt <mupbam1@gmail.com>
2023-05-05 12:10:04 +0300

    checkstyle typo fixed

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -16,11 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParserTest {
-    String jsonContent;
-    String expected;
-    String jsonArray;
-    String wrongArray;
-    HashMap<String, Object> expectedData;
+    private String jsonContent;
+    private String expected;
+    private String jsonArray;
+    private String wrongArray;
+    private HashMap<String, Object> expectedData;
 
     @BeforeEach
     void setUp() {
@@ -60,8 +60,8 @@ class ParserTest {
     void parseYaml() throws JsonProcessingException, ParseException {
         HashMap<Object, Object> expectedYaml = new HashMap<>();
         expectedYaml.put("martin", "{skill=Elite, name=Martin D'vloper, job=Developer}");
-        assertEquals(String.valueOf(expectedYaml), String.valueOf(Parser.parse("---\n" +
-                "martin: {name: Martin D'vloper, job: Developer, skill: Elite}")));
+        assertEquals(String.valueOf(expectedYaml), String.valueOf(Parser.parse("---\n"
+                + "martin: {name: Martin D'vloper, job: Developer, skill: Elite}")));
     }
     @Test
     void isJSONValid() {
@@ -77,17 +77,17 @@ class ParserTest {
     void getJson() throws JsonProcessingException, ParseException {
         String jsonArrayData = Reader.readFile(
                 "/home/timur/IdeaProjects/java-project-71/app/src/test/resources/array.json");
-        HashMap<Object, Object> expected = new HashMap<>();
-        expected.put("id", 1);
-        expected.put("title", "hello");
-        assertEquals(expected, Parser.getJson(jsonArrayData));
+        HashMap<Object, Object> expectedJsonData = new HashMap<>();
+        expectedJsonData.put("id", 1);
+        expectedJsonData.put("title", "hello");
+        assertEquals(expectedJsonData, Parser.getJson(jsonArrayData));
 
     }
 
     @Test
     void getJsonArray() throws JsonProcessingException {
         assertEquals(expected, String.valueOf(Parser.parseJsonArray(jsonArray)));
-        assertThrows(RuntimeException.class, ()-> String.valueOf(
+        assertThrows(RuntimeException.class, () -> String.valueOf(
                 Parser.parseJsonArray(wrongArray)));
     }
     @Test
@@ -99,15 +99,15 @@ class ParserTest {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
-        assertThrows(ParseException.class, ()-> String.valueOf(
+        assertThrows(ParseException.class, () -> String.valueOf(
                 Parser.getArrayDataMap((JSONArray) jsonParser.parse(wrongArray))));
     }
 
 
     @Test
     void getJsonObject() throws ParseException {
-        String expected = "{\"obj1\":{\"nestedKey\":\"value\",\"isNested\":true}}";
-        assertEquals(expected, String.valueOf(Parser.parseJsonObject("""
+        String expectedJson = "{\"obj1\":{\"nestedKey\":\"value\",\"isNested\":true}}";
+        assertEquals(expectedJson, String.valueOf(Parser.parseJsonObject("""
                 {"obj1": {
                     "nestedKey": "value",
                     "isNested": true
@@ -116,9 +116,9 @@ class ParserTest {
 
     @Test
     void convertYamlToJsonData() throws JsonProcessingException {
-        String expected = "{\"martin\":{\"name\":\"Martin D'vloper\","
+        String expectedYamlToJson = "{\"martin\":{\"name\":\"Martin D'vloper\","
                 + "\"job\":\"Developer\",\"skill\":\"Elite\"}}";
-        assertEquals(expected, Parser.convertYamlToJsonData("---\n" +
+        assertEquals(expectedYamlToJson, Parser.convertYamlToJsonData("---\n" +
                 "martin: {name: Martin D'vloper, job: Developer, skill: Elite}"));
     }
 
