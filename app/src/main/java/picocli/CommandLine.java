d7bdc86ffa1bcb25ee2bbe5f96b9814db74497fc
Daukaevt <mupbam1@gmail.com>
2023-05-10 08:08:47 +0300

    checkstyle typo fixed

--- app/src/main/java/picocli/CommandLine.java
+++ app/src/main/java/picocli/CommandLine.java
@@ -21800,7 +21800,7 @@ InitialValueState.CACHED;*/
                 for (Enumeration<String> k = rb.getKeys();
                      k.hasMoreElements();
                      keys.add(k.nextElement())) {
-                    //do nothing.
+                    ;
                 }
                 return keys;
             }
