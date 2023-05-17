4d689501caf2ce0126dfd93d7ab68712d324c429
Daukaevt <mupbam1@gmail.com>
2023-05-17 15:34:52 +0300

    checkstyle typo fixed

--- app/src/main/java/hexlet/code/formatter/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatter/StylishFormatter.java
@@ -11,12 +11,10 @@ public class StylishFormatter {
             String value1 = unitMap.get(key).getValue1();
             String value2 = unitMap.get(key).getValue2();
             if ("-absent-".equals(value1)) {
-                stringBuilder.append("  + ").append(key).append(": ")
-                        .append(value2).append("\n"); return;
+                stringBuilder.append("  + ").append(key).append(": ").append(value2).append("\n"); return;
             }
             if ("-absent-".equals(value2)) {
-                stringBuilder.append("  - ").append(key).append(": ")
-                        .append(value1).append("\n"); return;
+                stringBuilder.append("  - ").append(key).append(": ").append(value1).append("\n"); return;
             }
             if (!value1.equals(value2)) {
                 stringBuilder.append("  - ").append(key).append(": ")
