2bbd9e0363901440957f0c37ef5e8a6af8e73277
Daukaevt <mupbam1@gmail.com>
2023-05-23 13:38:56 +0300

    lineRemoved, lineAdded, sameLine methods added

--- app/src/test/java/hexlet/code/formatter/JsonFormatterTest.java
+++ app/src/test/java/hexlet/code/formatter/JsonFormatterTest.java
@@ -1,25 +1,53 @@
 package hexlet.code.formatter;
 
 import hexlet.code.utils.Wrapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class JsonFormatterTest {
+    private TreeMap<String, Wrapper> testMap;
+    private String expected;
 
-    @Test
-    void jsonFormat() {
-        TreeMap<String, Wrapper> testMap = new TreeMap<>();
+    JsonFormatterTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        testMap = new TreeMap<>();
         testMap.put("testKey", new Wrapper("value1", "value2"));
-        String expected = """
+        testMap.put("testKey1", new Wrapper("-absent-", "value21"));
+        testMap.put("testKey2", new Wrapper("value12", "-absent-"));
+        testMap.put("testKey3", new Wrapper("value33", "value33"));
+        expected = """
                 {
                   "- testKey": "value2",
-                  "+ testKey": "value1"
+                  "+ testKey": "value1",
+                  "- testKey1": "value21",
+                  "+ testKey2": "value12",
+                  "  testKey3": "value33"
                 }
                 """;
+    }
+    @Test
+    void jsonFormat() {
         assertEquals(expected, JsonFormatter.jsonFormat(testMap));
+    }
+    @Test
+    void lineRemoved() {
+        assertTrue(true, String.valueOf(JsonFormatter.jsonFormat(testMap).contains("- testKey1\": \"value21")));
+    }
+    @Test
+    void lineAdded() {
+        assertTrue(true, String.valueOf(JsonFormatter.jsonFormat(testMap).contains("+ testKey2\": \"value12")));
+    }
+    @Test
+    void sameLine() {
+        assertTrue(true, String.valueOf(JsonFormatter.jsonFormat(testMap).contains("  testKey3\": \"value33")));
     }
 }
