7905a0f2c71473f8f10f0790251016f0c550fd44
Daukaevt <mupbam1@gmail.com>
2023-05-28 13:34:54 +0300

    typo fixed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Callable;
         version = "1.0 beta",
         description = "Compares two configuration files and shows a difference."
 )
-public class App implements Callable <Integer> {
+public class App implements Callable<Integer> {
     private static final int SUCCESS_EXIT_CODE = 0;
     private static final int ERROR_EXIT_CODE = 1;
 
