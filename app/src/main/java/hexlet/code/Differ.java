5f0e778097838ce807b4045be84353823f114bb0
Daukaevt <mupbam1@gmail.com>
2023-06-14 20:44:22 +0300

    readContent method added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,6 +4,7 @@ package hexlet.code;
 import hexlet.code.formatter.ContentFormatter;
 import hexlet.code.utils.Wrapper;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,15 +29,20 @@ public class Differ {
             return "File does not exist";
         }
         Map<String, Object> parseJson1 =
-                Parser.parse(Reader.read(path1.toAbsolutePath().toString()),
+                Parser.parse(readContent(path1.toAbsolutePath().toString()),
                         getDataFormat(String.valueOf(path1)));
 
         Map<String, Object> parseJson2 =
-                Parser.parse(Reader.read(path2.toAbsolutePath().toString()),
+                Parser.parse(readContent(path2.toAbsolutePath().toString()),
                         getDataFormat(String.valueOf(path2)));
         TreeMap<String, Wrapper> unitMap = Uniter.unite(parseJson1, parseJson2);
         return ContentFormatter.makeFormat(unitMap, format);
     }
+
+    private static String readContent(String filePath) throws IOException {
+        return Files.readString(Path.of(filePath));
+    }
+
     static String getDataFormat(String filePath) {
         int index = filePath.lastIndexOf('.');
         return index > 0
