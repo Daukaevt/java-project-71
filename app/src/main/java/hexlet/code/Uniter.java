a0d2351677a21c44c9a3ecaea55c2398cbfc8a54
Daukaevt <mupbam1@gmail.com>
2023-06-30 18:24:18 +0300

    nestedData method removed

--- app/src/main/java/hexlet/code/Uniter.java
+++ app/src/main/java/hexlet/code/Uniter.java
@@ -1,67 +1,31 @@
 package hexlet.code;
 
 
-import com.google.common.base.Splitter;
 import hexlet.code.utils.Wrapper;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Uniter {
-    public static TreeMap unite(
-            final Map<String, Object> parseJson1,
-            final Map<String, Object> parseJson2
-    ) {
-        TreeMap<String, Wrapper> unitMap = new TreeMap<>();
-        for (Object key: parseJson1.keySet()) {
-            if (parseJson2.containsKey(String.valueOf(key))) {
-                unitMap.put(key.toString(), new Wrapper(
-                        (parseJson1.get(String.valueOf(key)).toString()),
-                        (parseJson2.get(String.valueOf(key)).toString())));
-            } else {
-                unitMap.put(key.toString(), new Wrapper(
-                        (parseJson1.get(String.valueOf(key)).toString()),
-                        ("-absent-")));
-            }
-        }
-        for (Object key: parseJson2.keySet()) {
-            if (!parseJson1.containsKey(String.valueOf(key))) {
-                unitMap.put(key.toString(), new Wrapper(
-                        ("-absent-"),
-                        (parseJson2.get(String.valueOf(key)).toString())));
-            }
-        }
-        return addWhiteSpaces(unitMap);
-    }
-    public static TreeMap addWhiteSpaces(TreeMap<String, Wrapper> unitMap) {
-        TreeMap<String, Wrapper> whiteSpacedUnitMap = new TreeMap<>();
-        for (String key: unitMap.keySet()) {
-            Wrapper values = unitMap.get(key);
-            if (values != null) {
-                whiteSpacedUnitMap.put(key,
-                        new Wrapper(nestedData(values.getValue1()), nestedData(values.getValue2())));
-            }
-        }
-        return whiteSpacedUnitMap;
-    }
-    static String nestedData(String values) {
-        if (values.startsWith("{") || values.endsWith("}")) {
-            return mappingValue(values);
-        } else if (values.startsWith("[{") || values.endsWith("}]")) {
-            return "[" + mappingValue(values.substring(1, values.length() - 1)) + "]";
-        } else if (values.startsWith("[") || values.endsWith("]")) {
-            String arrayData = Arrays.toString(values.split(","));
-            return arrayData.substring(1, arrayData.length() - 1);
-        } else {
-            return values;
-        }
-    }
 
-    private static String mappingValue(String values) {
-        Map<String, String> properties = Splitter.on(",")
-                .withKeyValueSeparator(":")
-                .split(values);
-        return String.valueOf(properties).replaceAll("^.|.$", "");
+    public static Map<String, Wrapper> matrix(Map<String, Object> parseJson1, Map<String, Object> parseJson2) {
+        Map<String, Wrapper> matrixKey1EqualsKey2 = parseJson1.entrySet().stream()
+                .filter(x -> parseJson2.containsKey(x.getKey()))
+                .collect(Collectors.toMap(
+                        x -> x.getKey(),
+                        x -> new Wrapper(x.getValue().toString(), parseJson2.get(x.getKey()).toString())));
+        Map<String, Wrapper> matrixKey1NotEqualsKey2 = parseJson1.entrySet().stream()
+                .filter(x -> !parseJson2.containsKey(x.getKey()))
+                .collect(Collectors.toMap(
+                        x -> x.getKey(),
+                        x -> new Wrapper(x.getValue().toString(), "-absent-")));
+        Map<String, Wrapper> matrixKey2NotEqualsKey1 = parseJson2.entrySet().stream()
+                .filter(x -> !parseJson1.containsKey(x.getKey()))
+                .collect(Collectors.toMap(
+                        x -> x.getKey(),
+                        x -> new Wrapper("-absent-", x.getValue().toString())));
+        matrixKey2NotEqualsKey1.putAll(matrixKey1EqualsKey2);
+        matrixKey2NotEqualsKey1.putAll(matrixKey1NotEqualsKey2);
+    return matrixKey2NotEqualsKey1;
     }
 }
