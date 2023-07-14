ce2de4d07f4831698a88fb55a208cdfde1e6d5ca
Daukaevt <mupbam1@gmail.com>
2023-07-14 23:01:45 +0300

    generics added to matrix method

--- app/src/main/java/hexlet/code/Uniter.java
+++ app/src/main/java/hexlet/code/Uniter.java
@@ -8,22 +8,25 @@ import java.util.stream.Collectors;
 
 public class Uniter {
 
-    public static Map<String, Wrapper> matrix(Map<String, Object> parseJson1, Map<String, Object> parseJson2) {
+    public static <K,V> Map<String, Wrapper> matrix(Map<? extends String, ? extends Object> parseJson1,
+            Map<? extends String, ? extends Object> parseJson2) {
+
         Map<String, Wrapper> matrixKey1EqualsKey2 = parseJson1.entrySet().stream()
                 .filter(x -> parseJson2.containsKey(x.getKey()))
                 .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        x -> new Wrapper(x.getValue().toString(), parseJson2.get(x.getKey()).toString())));
+                        stringObjectEntry -> stringObjectEntry.getKey(),
+                        x -> new Wrapper(String.valueOf(x.getValue()),
+                                String.valueOf(parseJson2.get(x.getKey())))));
         Map<String, Wrapper> matrixKey1NotEqualsKey2 = parseJson1.entrySet().stream()
                 .filter(x -> !parseJson2.containsKey(x.getKey()))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
-                        x -> new Wrapper(x.getValue().toString(), "-absent-")));
+                        x -> new Wrapper(String.valueOf(x.getValue()), "-absent-")));
         Map<String, Wrapper> matrixKey2NotEqualsKey1 = parseJson2.entrySet().stream()
                 .filter(x -> !parseJson1.containsKey(x.getKey()))
                 .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        x -> new Wrapper("-absent-", x.getValue().toString())));
+                        stringObjectEntry -> stringObjectEntry.getKey(),
+                        x -> new Wrapper("-absent-", String.valueOf(x.getValue()))));
         matrixKey2NotEqualsKey1.putAll(matrixKey1EqualsKey2);
         matrixKey2NotEqualsKey1.putAll(matrixKey1NotEqualsKey2);
         return matrixKey2NotEqualsKey1;
