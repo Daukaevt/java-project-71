eb96536276589b598348a75e59f4a98f1437a57f
Daukaevt <mupbam1@gmail.com>
2023-07-15 00:33:57 +0300

    typo fixed

--- app/src/main/java/hexlet/code/formatter/ContentFormatter.java
+++ app/src/main/java/hexlet/code/formatter/ContentFormatter.java
@@ -1,11 +1,8 @@
 package hexlet.code.formatter;
 
 
-import com.google.common.base.Splitter;
 import hexlet.code.utils.Wrapper;
 
-import java.util.Arrays;
-import java.util.Map;
 import java.util.TreeMap;
 
 
@@ -16,48 +13,15 @@ public class ContentFormatter {
     ) throws Exception {
         switch (format) {
             case ("plain") -> {
-                return PlainFormatter.plainFormat(addWhiteSpaces(unitMap));
+                return PlainFormatter.plainFormat(unitMap);
             }
             case ("json") -> {
-                return JsonFormatter.jsonFormat(addWhiteSpaces(unitMap));
+                return JsonFormatter.jsonFormat(unitMap);
             }
             case ("stylish") -> {
-                return StylishFormatter.stylishFormat(addWhiteSpaces(unitMap));
+                return StylishFormatter.stylishFormat(unitMap);
             }
-            default -> {
-                throw new Exception("Unknown format: " + format);
-            }
+            default -> throw new Exception("Unknown format: " + format);
         }
     }
-    public static TreeMap addWhiteSpaces(TreeMap<String, Wrapper> unitMap) {
-        TreeMap<String, Wrapper> whiteSpacedUnitMap = new TreeMap<>();
-        for (String key: unitMap.keySet()) {
-            Wrapper values = unitMap.get(key);
-            if (values != null) {
-                whiteSpacedUnitMap.put(key,
-                        new Wrapper(nestedData(values.getValue1()), nestedData(values.getValue2())));
-            }
-        }
-        return whiteSpacedUnitMap;
-    }
-    static String nestedData(String values) {
-        if (values.startsWith("{") || values.endsWith("}")) {
-            return mappingValue(values);
-        } else if (values.startsWith("[{") || values.endsWith("}]")) {
-            return "[" + mappingValue(values.substring(1, values.length() - 1)) + "]";
-        } else if (values.startsWith("[") || values.endsWith("]")) {
-            String arrayData = Arrays.toString(values.split(","));
-            return arrayData.substring(1, arrayData.length() - 1);
-        } else {
-            return values;
-        }
-    }
-
-    private static String mappingValue(String values) {
-        Map<String, String> properties = Splitter.on(",")
-                .withKeyValueSeparator(":")
-                .split(values);
-        return String.valueOf(properties).replaceAll("^.|.$", "");
-    }
-
 }
