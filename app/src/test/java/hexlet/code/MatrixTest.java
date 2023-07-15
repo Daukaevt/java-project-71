3c492d7adc09540215b6b3ae6da0b6476a22c0a6
Daukaevt <mupbam1@gmail.com>
2023-07-15 15:55:46 +0300

    typo fixed

---
+++ app/src/test/java/hexlet/code/MatrixTest.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import hexlet.code.utils.Wrapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixTest {
+    private String expected;
+    private Map<String, Wrapper> unitedData;
+    @BeforeEach
+    void setUp() {
+        expected = "-absent-";
+        HashMap<String, Object> firstData = new HashMap<>();
+        HashMap<String, Object> secondData = new HashMap<>();
+        firstData.put("profile", "{name:typicode}");
+        firstData.put("posts", "[{id:1,title:hello}]");
+        firstData.put("field", "null");
+        secondData.put("fieldNew", "valueNew");
+        unitedData = Matrix.createMatrix(firstData, secondData);
+    }
+
+    @Test
+    void unite()
+    {
+        assertEquals(expected, String.valueOf(unitedData.get("posts").getValue2()));
+    }
+
+}
