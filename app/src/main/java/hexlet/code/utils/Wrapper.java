add5cf068fa9152b09d2362bc0d4a40abba513a5
Daukaevt <mupbam1@gmail.com>
2023-08-07 21:12:59 +0300

    wrapper class rewrited to record

--- app/src/main/java/hexlet/code/utils/Wrapper.java
+++ app/src/main/java/hexlet/code/utils/Wrapper.java
@@ -1,17 +1,5 @@
 package hexlet.code.utils;
 
-public final class Wrapper {
-    public Wrapper(final String value1, final String value2) {
-        this.value1 = value1;
-        this.value2 = value2;
-    }
-
-    public String getValue1() {
-        return this.value1; }
-    public String getValue2() {
-        return this.value2; }
-
-    private final String value1;
-    private final String value2;
+public record Wrapper(String value1, String value2) {
 
 }
